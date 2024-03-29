c5cb30d87389287ae9cbe04ececfcce475095195
riqsans <thrq88@gmail.com>
2024-03-29 15:40:29 +0700

    step 5 -13

--- QuickSort_0100/QuickSort_0100.cpp
+++ QuickSort_0100/QuickSort_0100.cpp
@@ -56,4 +56,40 @@ void q_short(int low, int high)
 	j = high; //step 4
 
 
+	while (i <= j) //step 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) // step 5
+		{
+			i++; //step 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //step 7
+		{
+			j--; //step 8
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j) //step 9
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+		}
+	}
+
+	if (low < j) { //step 11
+		//swap the pivot element with the element at index j
+		swap(low, j);
+	}
+
+	//recursive call to sort the left sub array
+	q_short(low, j - 1); //step 12
+
+	//recursive call to sort the right sub array
+
+	q_short(j + 1, high); //step 13
+
+
 }
